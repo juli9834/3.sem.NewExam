9e746d122beaffe92c2c165b55f6540dc09246c3
TobiasJZagal <tobi4680@stud.kea.dk>
2019-08-05 18:08:05 +0200

    added build

---
+++ myapp/build/precache-manifest.0537c5fcccd4f3702c42ce6a4f08ae4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "693d28c73824177ce9ad",
+    "url": "/exam/minibingo/static/js/runtime~main.132b4484.js"
+  },
+  {
+    "revision": "1ff1ef7b5590af900533",
+    "url": "/exam/minibingo/static/js/main.3afaf89b.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/exam/minibingo/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1ff1ef7b5590af900533",
+    "url": "/exam/minibingo/static/css/main.c34f1ddb.chunk.css"
+  },
+  {
+    "revision": "5623c0a4bb93d262387dcf5587c10c1a",
+    "url": "/exam/minibingo/index.html"
+  }
+];
