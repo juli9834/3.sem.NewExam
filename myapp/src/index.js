17d25bb6b4abcc0ede79c4d63bc467027ffbbd64
TobiasJZagal <tobi4680@stud.kea.dk>
2019-08-05 18:05:05 +0200

    removed old files

--- myapp/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/fetchApp";
-
-ReactDOM.render(<App />, document.getElementById("root"));
